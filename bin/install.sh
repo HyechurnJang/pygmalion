bacfb99b4bc0221cb27879d939b3a54946ba6a76
jzide <jzide@JzSB>
2018-10-13 14:40:43 +0900

    update

--- bin/install.sh
+++ bin/install.sh
@@ -4,8 +4,8 @@ NFS_DRIVER_VER="0.34"
 
 usages() {
     echo "Usages : install.sh [master|worker]"
-    echo "    master : install master node"
-    echo "    worker : install worker node"
+    echo "    master <MASTER_NODE_IP> : install master node"
+    echo "    worker <MASTER_NODE_IP> <TOKEN> : install worker node"
     echo ""
     exit 1
 }
@@ -20,7 +20,15 @@ install_base() {
 }
 
 install_master() {
+    local master_ip=$1
+    local ips=`ip a | grep inet | awk '{print $2}' | sed -e "s/\/.*//g" | paste -sd"|"`
     echo "install master node"
+    if [ -z $master_ip ]; then
+        usages
+    fi
+    if ! [[ "$master_ip" =~ ($ips) ]]; then
+        usages
+    fi
     apt install -y nfs-kernel-server
     mkdir -p /opt/pygmalion
     mkdir -p /opt/pygmalion/workspace
@@ -30,17 +38,24 @@ EOF
     systemctl restart nfs-kernel-server
     systemctl enable nfs-kernel-server
     install_base
+    docker swarm leave --force > /dev/null
+    local token=`docker swarm init --advertise-addr $master_ip | grep "\-\-token" | awk '{print $2}'`
+    echo "install master node finished"
+    echo "do typing followed to install on worker"
+    echo "    install.sh 192.168.0.101 $token"
+    echo ""
 }
 
 install_worker() {
     echo "install worker node"
     install_base
+    docker swarm join --token SWMTKN-1-1la23f7y6y8joqxz27hac4j79yffyixcp15tjtlrnei14wwd1t-5tlmx4q9fm46drjzzd95g1l4q 172.17.8.101:2377
 }
 
 CMD=$1
 case $CMD in
     master)
-        install_master
+        install_master $2
         ;;
     worker)
         install_worker
